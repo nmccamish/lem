31f3f80bf3c1a94ad4ad3330541338ecb64bbc16
Konata <paul.rouquette19@gmail.com>
2026-08-26 16:43:40 +0200

    feat(typst-mode): Adding hooks to stop tinymist when we exit lem

--- extensions/typst-mode/preview/preview.lisp
+++ extensions/typst-mode/preview/preview.lisp
@@ -35,16 +35,32 @@
 
 (define-command typst-export-file (output-pdf)
     ((prompt-for-string "PDF Name : " :initial-value "out.pdf"))
-  (let ((input (buffer-filename (current-buffer))
-               cmd (append (list "typst" "compile" (namestring input))  
-                           (when (and *typst-root* (not (string= *typst-root* "")))
-                                                                          (list "--root" *typst-root*))
-                           (list output-pdf))))
-    (uiop:run-program cmd))
-  (message "Exported successfully in ~A" output-pdf))
+  (let* ((input (buffer-filename (current-buffer)))
+        (cmd (append (list "typst" "compile" (namestring input))  
+                     (when (and *typst-root* (not (string= *typst-root* "")))
+                       (list "--root" *typst-root*))
+                     (list (concatenate 'string (namestring (buffer-directory)) output-pdf)))))
+        (uiop:run-program cmd))
+    (message "Exported successfully in ~A" output-pdf))
                         
 
+(defun typst-cleanup-tinymist ()
+  "Kill active tinymist before closing lem"
+  (maphash (lambda (filepath session)
+             (declare (ignore filepath))
+             (let ((process (typst-preview-session-process session)))
+               (when (and proc (uiop:process-alive-p proc))
+                 (ignore-errors (uiop:terminate-process process :urgent t)))))
+           *typst-preview-sessions*)
+  (clrhash *typst-preview-sessions*))
 
+
+
+;; clean all tinymist process in case editor is closed
+(add-hook *exit-editor-hook* 'typst-cleanup-tinymist)
+
+#+sbcl
+(pushnew 'typst-cleanup-tinymist sb-ext:*exit-hooks*)
 
 (define-command typst-preview () ()
   "Preview the current Typst buffer in the browser.
@@ -53,16 +69,17 @@ Otherwise, starts a new tinymist preview server.
 We avoid relaunching tinymist by saving the url it uses for the preview."
   (let ((file (buffer-filename (current-buffer))))
     (if file
-        (let* ((filepath (namestring file))
-               (session (gethash filepath *typst-preview-sessions*)))
+        (let* ((filepath (namestring file))Ʉ
+                                           (session (gethash filepath *typst-preview-sessions*)))
           (cond
             ;; Server is already running: reopen the browser at the existing URL without restarting
+            
             ((and session
-                  (typst-preview-session-process session)
-                  (uiop:process-alive-p (typst-preview-session-process session)))
-             (let ((url (or (typst-preview-session-url session) "http://127.0.0.1:23625")))
-               (lem:open-external-file url)
-               (message "Reopened Typst preview at ~A" url)))
+                 (typst-preview-session-process session)
+                 (uiop:process-alive-p (typst-preview-session-process session)))
+            (let ((url (or (typst-preview-session-url session) "http://127.0.0.1:23625")))
+              (lem:open-external-file url)
+              (message "Reopened Typst preview at ~A" url)))
             ;; Server is not running: start a new tinymist preview instance
             (t
              (stop-preview-process filepath)
@@ -74,8 +91,8 @@ We avoid relaunching tinymist by saving the url it uses for the preview."
                     (url "http://127.0.0.1:23625")) ;; Maybe make this a variable so user can customise ?
                (setf (gethash filepath *typst-preview-sessions*)
                      (make-typst-preview-session :process proc
-                                           :url url
-                                           :filepath filepath))
+                                                 :url url
+                                                 :filepath filepath))
                (message "Started Typst preview for ~A" (file-namestring filepath))))))
         (editor-error "Current buffer is not associated with a saved file."))))
 
@@ -88,5 +105,5 @@ We avoid relaunching tinymist by saving the url it uses for the preview."
               (progn
                 (stop-preview-process filepath)
                 (message "Typst preview server stopped."))
-              (message "No active preview server for this file.")))
+              (message "No active preview server for this file. ~A" filepath)))
         (editor-error "Current buffer is not associated with a file."))))
